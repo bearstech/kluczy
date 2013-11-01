344a89307827c1f85f2333f4fa30f1061ecc58f1
Mathieu Lecarme <mathieu@garambrogne.net>
2013-11-01 15:39:03 +0100

    Python stuffs.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='kluczy',
+    version=0.1,
+    description='',
+    license='',
+    author='',
+    author_email='',
+    keywords=[],
+    url='',
+    install_requires=[],
+    packages=['kluczy'],
+    scripts=['scripts/kl-ca'],
+    classifiers=[],
+)
