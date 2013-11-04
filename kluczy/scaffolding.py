a12f3c0d568d84990820f0116b89d687e510ce07
Mathieu Lecarme <mathieu@garambrogne.net>
2013-11-04 21:30:48 +0100

    Everybody need an uuid.

--- kluczy/scaffolding.py
+++ kluczy/scaffolding.py
@@ -1,6 +1,11 @@
 import os.path
+import uuid
+import glob
+import os
 
 DEFAULT="""
+[Main]
+id=%s
 [SSL]
 key_size=2048
 digest=sha256
@@ -34,4 +39,6 @@ O=Kluczy
 def main():
     if not os.path.exists('config.ini'):
         with open('config.ini', 'w') as f:
-            f.write(DEFAULT)
+            f.write(DEFAULT % uuid.uuid1())
+        for rotten in glob.glob('*.crt') + glob.glob('*.key'):
+            os.unlink(rotten)
